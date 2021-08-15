f87507d963b0f2b4f93b140107c184d24d4f55aa
Dhruv282008 <66867893+Dhruv282008@users.noreply.github.com>
2021-08-15 09:59:52 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,15 @@
+class Atm():
+    def __init__(self, PIN):
+        self.PIN = PIN
+        self.uinput = input("Enter your PIN number: ")
+        
+        if(self.uinput != self.PIN):
+            print("Wrong Pin Number")
+
+        else:
+            uinput2 = self.uinput2 = input("Enter Your Bank Balance: ")
+            print(uinput2)
+            print(input("Enter Amount To Withdraw: "))
+            print("Money Withdrawn Successfully!")
+
+atm = Atm("1234")
